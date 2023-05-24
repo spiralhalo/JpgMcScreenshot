0b35a14b91f997d0a25a7abe1e97e8ed3f090769
spiralhalo <re.nanashi95@gmail.com>
2023-05-24 18:01:02 +0700

    Update deps, CTRL+F2 for PNG

--- src/main/java/spiralhalo/jpgscreenshot/JpgScreenshotMod.java
+++ src/main/java/spiralhalo/jpgscreenshot/JpgScreenshotMod.java
@@ -4,9 +4,15 @@ import net.fabricmc.api.ClientModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.mojang.blaze3d.platform.InputConstants;
+
+import net.minecraft.client.KeyMapping;
+
 public class JpgScreenshotMod implements ClientModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("jpgscreenshot");
     public static final int QUALITY = 80; // 1-100
+
+    public static final KeyMapping PNG_HOLD_DOWN = new KeyMapping("key.jpgscreenshot.png_hold_down", InputConstants.KEY_LCONTROL, "key.jpgscreenshot.category");
 
     @Override
     public void onInitializeClient() {
--- src/main/java/spiralhalo/jpgscreenshot/mixin/MixinScreenshot.java
+++ src/main/java/spiralhalo/jpgscreenshot/mixin/MixinScreenshot.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Screenshot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import spiralhalo.jpgscreenshot.JpgScreenshotMod;
 
 import java.io.File;
 
@@ -11,6 +12,10 @@ import java.io.File;
 public class MixinScreenshot {
     @Redirect(method = "getFile", at = @At(value = "NEW", target = "java/io/File" ), require = 1)
     private static File createFile(File parent, String child) {
-        return new File(parent, child.replace(".png", ".jpg"));
+        if (JpgScreenshotMod.PNG_HOLD_DOWN.isDown()) {
+            return new File(parent, child);
+        } else {
+            return new File(parent, child.replace(".png", ".jpg"));
+        }
     }
 }
